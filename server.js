cae5cdcd485c1844c63066bec2a4efded2494d59
nitrophenol <mayanksingh0713@gmail.com>
2023-10-14 01:28:18 +0530

    final commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app=express();
 const port=process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
-      res.send("this is an CI/CD pipeline");
+      res.send("this is an CI/CD pipeline test1");
 })
 app.listen(port,()=>{
       console.log(`Server is running at port ${port}`);
